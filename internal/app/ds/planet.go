78725ddf6fbe31bf6274af2fb0397e1fe6245c92
Muka <dima040805@yandex.ru>
2025-09-21 01:45:06 +0300

    sql fix

--- internal/app/ds/planet.go
+++ internal/app/ds/planet.go
@@ -3,11 +3,11 @@ package ds
 type Planet struct {
 	ID          int `gorm:"primaryKey"`
 	IsDelete    bool   `gorm:"type:boolean not null;default:false"`
-	Image       string
+	Image       string `gorm:"type: varchar(32)"`
 	Name        string `gorm:"type:varchar(25);not null"`
 	Distance    int
-	Description string
-	Mass        float64
+	Description string `gorm:"type: varchar(512)"`
+	Mass        float64 `gorm:"type: float"`
 	Discovery   int
 	StarRadius  int
 }
--- internal/app/ds/research.go
+++ internal/app/ds/research.go
@@ -1,15 +1,20 @@
 package ds
 
 import (
+	"database/sql"
 	"time"
 )
 
 type Research struct {
 	ID           uint   `gorm:"primaryKey"`
-	Status       string `gorm:"type:varchar(15);not null"`
 	DateResearch time.Time
+	Status       string `gorm:"type:varchar(15);not null"`
 	DateCreate   time.Time `gorm:"not null"`
+	DateForm    sql.NullTime  `gorm:"default:null"`
+	DateFinish  sql.NullTime  `gorm:"default:null"`
 	CreatorID    int       `gorm:"not null"`
+	ModeratorID sql.NullInt64 `gorm:"default:null"`
 
 	Creator Users `gorm:"foreignKey:CreatorID"`
+	Moderator Users `gorm:"foreignKey:ModeratorID"`
 }
